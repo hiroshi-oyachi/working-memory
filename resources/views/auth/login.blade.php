59773e6ac7ad7f7626c6593671f460ea95814d90
hiroshi oyachi <167donmai@gmail.com>
2023-05-18 06:06:10 +0000

    修正

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,6 +5,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="login-box card">
+                    <div class="login-header card-header mx-auto">仕事記録アプリへようこそ</div>
                     <div class="login-header card-header mx-auto">{{ __('messages.login') }}</div>
 
                     <div class="login-body card-body">
